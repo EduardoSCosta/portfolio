5d520bdbb1f1576e6b409062597b4c090f1421da
EduardoSCosta <eduscaraujo@gmail.com>
2026-08-16 16:23:14 -0300

    Add next-intl i18n for en and pt-BR

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="hero">
-      <h1 className="hero__name">Eduardo</h1>
-      <p className="hero__tagline">Software developer</p>
-    </main>
-  );
-}
